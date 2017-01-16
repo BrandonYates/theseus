7d0c313a3e06bfe510be5024d47a3b1d2d76c077
BrandonYates <brandonyates66@gmail.com>
2017-01-15 19:27:20 -0600

    minor bug fix

--- daedalus/Maze.py
+++ daedalus/Maze.py
@@ -5,7 +5,6 @@ class Maze(object):
     def __init__(self, maze):
         self.maze = maze
 
-
     @classmethod
     def build(cls, length, width, sections, start, end):
 
@@ -22,13 +21,13 @@ class Maze(object):
     #path is an array of Section objects    
     def correctPath(self, path):
 
-        correctStart = path[0] == self.start
+        correctStart = (path[0] == self.start)
 
         #exit early if the path does not begin and end  at the correct section
         if not correctStart:
             return False
 
-        correctEnd = path[path.len] == self.end
+        correctEnd = (path[path.len] == self.end)
 
         if not correctEnd:
             return False
@@ -36,10 +35,14 @@ class Maze(object):
         #iterate through transitions and return false if any transition is not valid
         #any path made up of a sequence of valid transitions which begins and ends
         #at the correct start and end is considered a valid path
-        for step in range(0, path.len -1):
-            if not Section.validTransition(path[step], path[step + 1]):
-                return False
 
+        sections = self.maze["sections"] 
+        for step in range(0, path.len - 1):
+            s1 = sections[path[step][0], path[step][1]]
+            s2 = sections[path[step + 1][0], path[step + 1][1]]
+            
+            if not Section.validTransition(s1, s2):
+                return False
 
         return True                
 
